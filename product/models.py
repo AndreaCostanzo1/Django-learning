8216618547f87f24ab857da352e37be87b60ff33
AndreaCostanzo1 <andrea1.costanzo@mail.polimi.it>
2021-01-08 12:34:30 +0100

    Performed simple migration

--- product/models.py
+++ product/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Product(models.Model):
     title       = models.TextField()
     description = models.TextField()
-    price       = models.TextField()
+    price       = models.DecimalField(max_digits=10, decimal_places=2)
