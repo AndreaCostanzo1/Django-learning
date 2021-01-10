099e63bc12a3171397987cf3d5b3b17f8243a8a1
AndreaCostanzo1 <andrea1.costanzo@mail.polimi.it>
2021-01-10 22:03:22 +0100

    Displaying a list of items

--- product/views.py
+++ product/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 def home_view(request, *args,**kwargs):
     my_context= {
-        "my_text" : "static context"
+        "my_list" : ["element 1", "element 2", "element 3"]
     }
     return render(request, "home.html", my_context)
