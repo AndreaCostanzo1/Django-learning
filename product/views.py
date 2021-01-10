75606a9bc67f48266989a54d54070dc7639803c1
AndreaCostanzo1 <andrea1.costanzo@mail.polimi.it>
2021-01-10 12:36:02 +0100

    Added static context to the home template

--- product/views.py
+++ product/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_view(request, *args,**kwargs):
-    return render(request, "home.html", {})
+    my_context= {
+        "my_text" : "static context"
+    }
+    return render(request, "home.html", my_context)
