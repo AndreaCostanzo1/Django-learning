0cc1695a0042ec415997ebfaa93d2de0117bb8f7
AndreaCostanzo1 <andrea1.costanzo@mail.polimi.it>
2021-01-10 11:55:22 +0100

    Refactor: from Http response to django template

--- product/views.py
+++ product/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
-def home_view(*args,**kwargs):
-    return HttpResponse("<h1>Home</h1>")
+def home_view(request, *args,**kwargs):
+    return render(request, "home.html", {})
