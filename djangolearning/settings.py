7a2891e28c9fb97b5cc2ad7d1dd167a6777d6171
AndreaCostanzo1 <andrea1.costanzo@mail.polimi.it>
2021-01-11 10:45:12 +0100

    Added react as front-end

--- djangolearning/settings.py
+++ djangolearning/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-
+            os.path.join(BASE_DIR, 'frontend/build')
          ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -129,3 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/build/static')
+]
